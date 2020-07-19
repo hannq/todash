9dd2d5e584793c1bc4a31ef43a3239c09f47df4e
hannq <1094489295@qq.com>
2020-07-19 15:32:07 +0800

    增加测试代码

--- examples/src/index.ts
+++ examples/src/index.ts
@@ -3,4 +3,8 @@ import _ from 'todash';
 
 type testTuple = ['vue', 'react', 'angular', { vue: string }, { react: number }, { angular: boolean }];
 
+type ccc = { a: string } & { a: string | boolean }
+
+Object.assign
+
 type cc = _.TupleByConcat<testTuple, ['hello', 'world']>
